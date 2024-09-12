63621a1f3c8b5377a0cca49594e71b1711ac0813
Rishav Mukherjee <rishavmukherjee11@gmail.com>
2024-09-13 00:30:12 +0530

    packages setting up and tailwind works

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Stack } from 'expo-router';
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className=" flex-1 justify-center bg-white">
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
