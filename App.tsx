2b3302e88a34e1a4283042d8a8af7aa9de27068d
Khushi Jaiswal <khushijaiswal0600@gmail.com>
2024-09-17 01:46:39 +0530

    adding User details page

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import RootNav from './src/navigation/RootNav';
 import { Provider } from 'react-redux';
 
 import { store } from './src/utils/Store';  // Import your store
-import ContactUs from './src/screens/ContactUs';
+
 // Define the stack navigator
 const Stack = createStackNavigator();
 
@@ -31,7 +31,8 @@ export default function App() {
     );
   }
 
-  return (<Provider store={store}>
+  return (
+    <Provider store={store}>
     <RootNav/>
     </Provider>
   );
